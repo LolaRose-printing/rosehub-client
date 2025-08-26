86753b0594a66abe26f591fec9a3e406146f6023
SartorFit Inc <143529618+TKpopoola-consulting@users.noreply.github.com>
2025-08-26 14:20:16 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
+"use client";
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { AuthProvider } from '@/components/AuthProvider';
+import { Auth0Provider } from "@auth0/auth0-react";
 import "./globals.css";
 
 const inter = Inter({
@@ -21,16 +23,22 @@ export default function RootLayout({
   children: React.ReactNode;
   modal: React.ReactNode;
 }>) {
+  const domain = process.env.NEXT_PUBLIC_AUTH0_DOMAIN!;
+  const clientId = process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID!;
+  const audience = "https://server.lolaprint.us/api";
+
   return (
     <html lang="en">
-      <body
-        className={`${inter.className} antialiased`}
-      >
-        <AuthProvider>
+      <body className={`${inter.className} antialiased`}>
+        <Auth0Provider
+          domain={domain}
+          clientId={clientId}
+          authorizationParams={{ redirect_uri: typeof window !== "undefined" ? window.location.origin : undefined, audience }}
+        >
           {modal}
           <div id="modal-root" />
           {children}
-        </AuthProvider>
+        </Auth0Provider>
       </body>
     </html>
   );
