e6aa5907f6da8b117fa0f22369d86fd66c464fa6
SartorFit Inc <143529618+TKpopoola-consulting@users.noreply.github.com>
2025-08-27 14:11:15 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import AuthProvider from "./AuthProvider"; 
-//import { Auth0Provider } from "@auth0/auth0-react";
-
+import { AuthProvider } from '@/components/AuthProvider';
 import "./globals.css";
 
 const inter = Inter({
