1ca29348ff09d0fbe389698cfd036d1c364c3cd8
SartorFit Inc <143529618+TKpopoola-consulting@users.noreply.github.com>
2025-09-01 13:19:25 -0500

    Update Auth0Wrapper.tsx

--- src/components/Auth0Wrapper.tsx
+++ src/components/Auth0Wrapper.tsx
@@ -1,7 +1,11 @@
 "use client";
+import { useUser } from "@auth0/nextjs-auth0/client";
 
-import { UserProvider } from "@auth0/nextjs-auth0";
+export default function MyComponent() {
+  const { user, error, isLoading } = useUser();
 
-export default function Auth0Wrapper({ children }: { children: React.ReactNode }) {
-  return <UserProvider>{children}</UserProvider>;
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  return <div>Welcome, {user?.name}</div>;
 }
