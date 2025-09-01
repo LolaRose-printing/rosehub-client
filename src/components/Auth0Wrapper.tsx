d635af21240b75e8cbeb810bab42f676ef421a8b
SartorFit Inc <143529618+TKpopoola-consulting@users.noreply.github.com>
2025-09-01 17:49:12 -0500

    Update Auth0Wrapper.tsx

--- src/components/Auth0Wrapper.tsx
+++ src/components/Auth0Wrapper.tsx
@@ -5,14 +5,15 @@ import { Auth0Provider } from "@auth0/auth0-react";
 export default function Auth0Wrapper({ children }: { children: React.ReactNode }) {
   const domain = process.env.NEXT_PUBLIC_AUTH0_DOMAIN!;
   const clientId = process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID!;
-  const audience = "https://server.lolaprint.us/api";
+  const audience = process.env.NEXT_PUBLIC_AUTH0_AUDIENCE!;
+  const redirectUri = process.env.NEXT_PUBLIC_AUTH0_REDIRECT_URI!; // exact match
 
   return (
     <Auth0Provider
       domain={domain}
       clientId={clientId}
       authorizationParams={{
-        redirect_uri: typeof window !== "undefined" ? window.location.origin : "",
+        redirect_uri: redirectUri,
         audience,
       }}
     >
