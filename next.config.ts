c62f855985cf2fb16b516f3f556451005a24b91d
SartorFit Inc <143529618+TKpopoola-consulting@users.noreply.github.com>
2025-09-01 20:54:29 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,17 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
-    domains: ["localhost", "20.65.161.118", "server.lolaprint.us", "s.gravatar.com", "cdn.auth0.com"],
+    domains: [
+      "localhost",
+        "server.lolaprint.us",
+      "s.gravatar.com",
+      "cdn.auth0.com",
+    ],
   },
 
   publicRuntimeConfig: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-  }
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
